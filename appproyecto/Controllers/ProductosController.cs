7e86ad993288e5dc0161a8ccbab78a0d3e6b47b7
steven gazo <steven.gazo@hotmail.com>
2023-08-19 19:17:01 -0600

    cambios y ejemplso

---
+++ appproyecto/Controllers/ProductosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace appproyecto.Controllers
+{
+    public class ProductosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Buscar()
+        {
+            return View();
+        }
+        public IActionResult VerProducto()
+        {
+            return View();
+        }
+    }
+}
