734eb4d1465bc4bd7120053cb6fc44a218756a5b
Genesis <gbogantes23@gmail.com>
2023-08-19 21:00:21 -0600

    la bichota estuvo aqui

---
+++ appproyecto/Controllers/CategoriasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace appproyecto.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        public IActionResult Categorias()
+        {
+            return View();
+        }
+    }
+}
