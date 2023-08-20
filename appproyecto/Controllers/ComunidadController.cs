c1dab987cda3817028e2e06ce781cf3f49e134cf
steven gazo <steven.gazo@hotmail.com>
2023-08-19 22:52:24 -0600

    mae  me odio

---
+++ appproyecto/Controllers/ComunidadController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace appproyecto.Controllers
+{
+    public class ComunidadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Buscar()
+        {
+            return View();
+        }
+
+        public IActionResult Ver()
+        {
+            return View();
+        }
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ appproyecto/Controllers/PublicacionController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace appproyecto.Controllers
+{
+    public class PublicacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+        public IActionResult Ver()
+        {
+            return View();
+        }
+    }
+}
